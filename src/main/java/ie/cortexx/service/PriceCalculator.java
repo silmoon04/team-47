7f6882a2bfab170a53d467cff965e860380b01ff
rohanparmar1806 <rohan.parmar.2@city.ac.uk>
2026-03-28 21:01:27 +0000

    built price calculator, sale service, validation result, made getter and setter for saleitem sale customer

--- src/main/java/ie/cortexx/service/PriceCalculator.java
+++ src/main/java/ie/cortexx/service/PriceCalculator.java
@@ -1,5 +1,10 @@
 package ie.cortexx.service;
 
+import ie.cortexx.model.SaleItem;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 // all the money math lives here, static methods, no state
 // always use BigDecimal for money, never double
 //
@@ -7,10 +12,59 @@ package ie.cortexx.service;
 // BigDecimal does exact decimal math
 public class PriceCalculator {
 
-    // TODO: calculateRetailPrice(wholesale, markupRate) -> BigDecimal
-    // TODO: calculateVAT(price, vatRate) -> BigDecimal
-    // TODO: calculateLineTotal(quantity, retailPrice) -> BigDecimal
-    // TODO: calculateChange(tendered, totalDue) -> BigDecimal
-    // TODO: calculateDiscountAmount(price, discountRate) -> BigDecimal
-    // TODO: calculateTotalWithVAT(subtotal, vatAmount) -> BigDecimal
+    // TODO: calculateRetailPrice(wholesale, markupRate) -> BigDecimal //done
+    // TODO: calculateVAT(price, vatRate) -> BigDecimal //done
+    // TODO: calculateLineTotal(quantity, retailPrice) -> BigDecimal //done
+    // TODO: calculateChange(tendered, totalDue) -> BigDecimal //done
+    // TODO: calculateDiscountAmount(price, discountRate) -> BigDecimal //done
+    // TODO: calculateTotalWithVAT(subtotal, vatAmount) -> BigDecimal //done
+
+    //test commit
+    private static final BigDecimal VAT_RATE = new BigDecimal("0.23");
+
+    public static BigDecimal calculateLineTotal(int quantity, BigDecimal retailPrice){
+        BigDecimal lineTotal = retailPrice.multiply(BigDecimal.valueOf(quantity));
+        return lineTotal;
+    }
+
+    public static BigDecimal calculateSubtotal(List<SaleItem> items) {
+        BigDecimal subtotal = BigDecimal.ZERO;
+        for (SaleItem item : items) {
+            BigDecimal lineTotal = calculateLineTotal(item.getQuantity(), item.getUnitPrice());
+
+            subtotal = subtotal.add(lineTotal);
+        }
+        return subtotal;
+
+    }
+
+    public static BigDecimal calculateRetailPrice(BigDecimal wholesalePrice, BigDecimal markupRate){
+        BigDecimal markup = wholesalePrice.multiply(markupRate);
+        BigDecimal retailPrice = wholesalePrice.add(markup);
+        return retailPrice;
+    }
+
+    public static BigDecimal calculateDiscountAmount(BigDecimal price, BigDecimal discountRate) {
+        BigDecimal discount = price.multiply(discountRate);
+        return discount;
+
+
+    }
+
+    public static BigDecimal calculateVAT(BigDecimal price, BigDecimal vatRate ){
+    BigDecimal vat= price.multiply(vatRate);
+    return vat;
+    }
+
+    public static BigDecimal calculateTotalWithVAT(BigDecimal subtotal, BigDecimal vatAmount) {
+        BigDecimal totalWithVAT = subtotal.add(vatAmount);
+        return totalWithVAT;
+    }
+    public static BigDecimal calculateChange(BigDecimal tendered, BigDecimal totalDue){
+    BigDecimal change = tendered.subtract(totalDue);
+    return change;
+    }
+
+
+
 }
