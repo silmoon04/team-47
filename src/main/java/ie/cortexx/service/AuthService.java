95931ed6a3822f650473144ae7eeaf127e9d1483
Christian Asenov <caasenov@gmail.com>
2026-03-29 23:14:02 +0100

    update AuthService
    - created placeholder for auth service

--- src/main/java/ie/cortexx/service/AuthService.java
+++ src/main/java/ie/cortexx/service/AuthService.java
@@ -3,10 +3,31 @@ package ie.cortexx.service;
 // handles login and logout
 // hashes password with SHA-256 then checks against UserDAO
 // on success, stores the user in SessionManager
-public class AuthService {
 
-    // TODO: constructor taking UserDAO
-    // TODO: authenticate(username, password) -> boolean
-    // TODO: logout()
-    // TODO: static hashPassword(String) -> String (SHA-256)
+// TODO: constructor taking UserDAO
+// TODO: authenticate(username, password) -> boolean
+// TODO: logout()
+// TODO: static hashPassword(String) -> String (SHA-256)
+
+public class AuthService {
+    // placeholder variables
+    private String username;
+    private String password;
+
+    // placeholder constructor
+    public AuthService() {
+
+    }
+
+    // placeholder method
+    public boolean authenticate(String username, String password) {
+        this.username = username;
+        this.password = password;
+        return true;
+    }
+
+    // placeholder method
+    public static void hashPassword(String password) {
+
+    }
 }
